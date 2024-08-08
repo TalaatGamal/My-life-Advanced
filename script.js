48ad940b592a41b314302b772ae046baa981a367
V-User1 <talaatgamal890@gmail.com>
2024-08-08 17:52:50 +0300

    first commit

--- script.js
+++ script.js
@@ -29,3 +29,50 @@
     document.body.style.display = 'block'; // عرض نموذج كلمة المرور
     });
 
+                                            
+                                            
+                                            // private btn header
+                                            
+                                            
+
+  document.addEventListener('DOMContentLoaded', (event) => {
+    const protectedLinks = document.querySelectorAll('.pro2');
+    const correctPassword = 'porn'; // حدد كلمة السر الصحيحة هنا
+
+    protectedLinks.forEach(link => {
+      link.addEventListener('click', (event) => {
+        event.preventDefault(); // لمنع التحويل التلقائي للصفحة
+        const userPassword = prompt('Please enter the password:');
+        
+        if (userPassword === correctPassword) {
+          window.location.href = link.href; // التحويل للصفحة المطلوبة
+        } else {
+          alert('Incorrect password!'); // عرض رسالة خطأ
+        }
+      });
+    });
+  });
+
+                                            
+                                            
+                                            // private btn mobile
+                                            
+                                            
+
+  document.addEventListener('DOMContentLoaded', (event) => {
+    const protectedLinks = document.querySelectorAll('.pro');
+    const correctPassword = 'porn'; // حدد كلمة السر الصحيحة هنا
+
+    protectedLinks.forEach(link => {
+      link.addEventListener('click', (event) => {
+        event.preventDefault(); // لمنع التحويل التلقائي للصفحة
+        const userPassword = prompt('Please enter the password:');
+        
+        if (userPassword === correctPassword) {
+          window.location.href = link.href; // التحويل للصفحة المطلوبة
+        } else {
+          alert('Incorrect password!'); // عرض رسالة خطأ
+        }
+      });
+    });
+  });
